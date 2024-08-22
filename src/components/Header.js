896b377eae54cad4fcedd0d035123268a759b12d
AnoniPlay <104880152+AnoniPlay@users.noreply.github.com>
2024-08-22 17:36:19 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import React from 'react'
 const Header = () => (
   <header className='mb3 h5'>
     <div>♥️♠️♦️♣️️</div>
-    <h1 className='m0 h2'>Hi-Lo Practice</h1>
+    <h1 className='m0 h2'>Wong-Halves Practice</h1>
     <p className='m0'>
-      Hi-Lo is a simple card counting system used in blackjack.
-      It assigns a value of +1, 0, or -1 for every card that's played:
-      2-6 → +1, 7-9 → 0, 10-A → -1.
+      Wong-Halves is a simple card counting system used in blackjack.
+      It assigns a value of +1,+2,+3 0, or -1,-2 for every card that's played:
+      2,7 → +1, 3,4,6 → +2, 5 → +3, 8 → 0, 9 → 0,  10-A → -2.
     </p>
   </header>
 )
